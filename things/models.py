f94332fa3650c8e10001abdfa5dc0be0bf56fa82
Ibrahim Uddin <k21043852@kcl.ac.uk>
2023-10-04 16:40:41 +0100

    attempt 2

--- things/models.py
+++ things/models.py
@@ -1,17 +1,12 @@
 from django.db import models
-from django.core.validators import RegexValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(unique=True, blank=False, max_length=30)
     description = models.TextField(unique=False, max_length=120)
-    quantity = models.IntegerField(
-        unique=False,
-        validators=[
-            RegexValidator(
-                regex=r"^(100|[1-9]?[0-9])$",
-                message="must be a value between 0 and 100",
-                ),
-                ], 
-                )
+    quantity = models.IntegerField(unique=False, validators =[
+        MinValueValidator(0,message="Quantity must be greater than or equal to 0"),
+        MaxValueValidator(100,message="Quantity must be less than or equal to 100")
+    ])
     
 
