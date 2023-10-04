7686dee67f20daf43fd7df253d7daaf9b6d91258
Ibrahim Uddin <k21043852@kcl.ac.uk>
2023-10-04 16:33:42 +0100

    task 1 first attempt

--- things/models.py
+++ things/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(unique=True, blank=False, max_length=30)
+    description = models.TextField(unique=False, max_length=120)
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[
+            RegexValidator(
+                regex=r"^(100|[1-9]?[0-9])$",
+                message="must be a value between 0 and 100",
+                ),
+                ], 
+                )
     
+
