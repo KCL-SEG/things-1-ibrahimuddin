5bff18f9c14671db0c92010f87320ab86f6bb0a6
ibrahimuddin <100637739+ibrahimuddin@users.noreply.github.com>
2023-12-06 22:08:18 +0000

    task 3

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(unique=True, blank=False, max_length=30)
-    description = models.TextField(max_length=120, unique=False)
+    description = models.TextField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(unique=False, validators =[
         MinValueValidator(0,message="Quantity must be greater than or equal to 0"),
         MaxValueValidator(100,message="Quantity must be less than or equal to 100")
