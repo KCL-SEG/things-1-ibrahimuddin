671b06fc7a229e0dde98d6a265d23facf7ac3f3c
ibrahimuddin <100637739+ibrahimuddin@users.noreply.github.com>
2023-12-06 22:15:02 +0000

    task 3

--- things/models.py
+++ things/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator,MaxLengthValidator
+
 
 class Thing(models.Model):
     name = models.CharField(unique=True, blank=False, max_length=30)
-    description = models.TextField(max_length=120, unique=False, blank=True)
+    description = models.TextField(
+        max_length=120,
+        unique=False,
+        blank=True,
+        validators=[MaxLengthValidator(limit_value=120, message="Description must not have more than 120 characters")]
+    )
     quantity = models.IntegerField(unique=False, validators =[
         MinValueValidator(0,message="Quantity must be greater than or equal to 0"),
         MaxValueValidator(100,message="Quantity must be less than or equal to 100")
